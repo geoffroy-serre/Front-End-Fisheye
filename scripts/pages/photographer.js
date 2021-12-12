45bc24c15059b0d4b3c80d00c981d702727fe7b4
Geff <viva@privacy.unv>
2021-12-12 18:01:28 +0100

    working on dropdown filter menu. event listeners for option dont work yet

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,13 +1,14 @@
 //Mettre le code JavaScript lié à la page photographer.html
 import {closeModal, displayModal} from '../utils/contactForm.js';
 import {
-	DATA_FILE_PATH,
-	DATA_FILE_NAME,
+	// DATA_FILE_PATH,
+	// DATA_FILE_NAME,
 	PHOTOGRAPHERS_ID_PICTURES_PATH,
 } from '../utils/variables.js';
 
 import {getPhotographers, getMedias} from '../utils/retrieveData.js';
 
+// DOM Elements
 const openModalButton = document.querySelector('.contact_button');
 const closeModalButton = document.querySelector('.close_button');
 const modalBlock = document.getElementById('contact_modal');
@@ -32,13 +33,50 @@ async function init() {
 	const medias = await getMedias();
 
 	populateInfos(photographers);
+	filterMenu('popularité');
 	populateMedias(medias);
+}
+
+function filterMenu(selectedOption) {
+	const select = document.querySelector('.select');
+	const selectOptionElements = document.querySelectorAll('.select__option');
+	const filterOptions = ['Popularité', 'Date', 'Titre'];
+	let currentOption = selectedOption;
+	const otherOptions = [];
+
+	select.addEventListener('click', () => {
+		document.querySelector('.select__options').style.display = 'block';
+	});
+
+	filterOptions.forEach((option) => {
+		console.log(option);
+		if (option.toLowerCase() != currentOption.toLowerCase()) {
+			otherOptions.push(option);
+		} else if (option.toLowerCase() == currentOption.toLowerCase()) {
+			currentOption = option;
+		}
+	});
+
+	selectOptionElements.forEach((e) => {
+		console.log(e);
+		e.addEventListener('click', () => {
+			console.log('bite');
+		});
+	});
+
+	const selectFilter = `
+    <div class="select__current">${currentOption}</div>
+	<ul class="select__options">
+    <li class="select__option">${otherOptions[0]}</li>
+    <li class="select__option">${otherOptions[1]}</li>
+    </ul>
+    `;
+	select.innerHTML = selectFilter;
 }
 
 async function populateInfos(photographers) {
 	photographers.forEach((photographer) => {
 		if (urlParameter == photographer.id) {
-			console.log(`found ${photographer.id}`);
 			photographerId.setAttribute(
 				'src',
 				`${PHOTOGRAPHERS_ID_PICTURES_PATH}/${photographer.portrait}`
@@ -51,9 +89,11 @@ async function populateInfos(photographers) {
 }
 
 async function populateMedias(medias) {
-	medias.forEach((media) => {
-		if (media.photographerId == urlParameter) {
-			console.log(media);
-		}
-	});
+	medias = 'bite';
+	return medias;
+	// medias.forEach((media) => {
+	// 	if (media.photographerId == urlParameter) {
+	// 		console.log(media);
+	// 	}
+	// });
 }
